c6ebe86ad2a4d69b34dce12102b11f44fa978596
Sergei Iaskov <sergeiiaskov@microsoft.com>
2022-10-12 21:09:56 +0300

    Revert changes to default arguments values.

--- pkg/ngrok/server/cli.go
+++ pkg/ngrok/server/cli.go
@@ -16,10 +16,10 @@ type Options struct {
 }
 
 func parseArgs() *Options {
-	httpAddr := flag.String("httpAddr", ":9480", "Public address for HTTP connections, empty string to disable")
-	httpsAddr := flag.String("httpsAddr", ":9443", "Public address listening for HTTPS connections, emptry string to disable")
+	httpAddr := flag.String("httpAddr", ":80", "Public address for HTTP connections, empty string to disable")
+	httpsAddr := flag.String("httpsAddr", ":443", "Public address listening for HTTPS connections, emptry string to disable")
 	tunnelAddr := flag.String("tunnelAddr", ":4443", "Public address listening for ngrok client")
-	domain := flag.String("domain", "ngrok.cofire.cn", "Domain where the tunnels are hosted")
+	domain := flag.String("domain", "ngrok.com", "Domain where the tunnels are hosted")
 	tlsCrt := flag.String("tlsCrt", "", "Path to a TLS certificate file")
 	tlsKey := flag.String("tlsKey", "", "Path to a TLS key file")
 	logto := flag.String("log", "stdout", "Write log messages to this file. 'stdout' and 'none' have special meanings")
